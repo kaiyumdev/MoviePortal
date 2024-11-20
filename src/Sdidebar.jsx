65204d26c05bf97af1803e00a7d646b92f08c8d3
kaiyumdev <afrankaiyum34@gmail.com>
2024-11-20 21:18:21 +0600

    feat(styles): add styles to the app component

--- src/Sdidebar.jsx
+++ src/Sdidebar.jsx
@@ -1,34 +1,39 @@
+import Treding from "../src/assets/icons/trending.svg";
+import NewRelease from "../src/assets/icons/newRelease.svg";
+import CommingSoon from "../src/assets/icons/commingSoon.svg";
+import Favourite from "../src/assets/icons/favourite.svg";
+import WatchLater from "../src/assets/icons/watchLater.svg";
 const Sdidebar = () => {
     return (
         <aside>
             <ul className="space-y-2">
                 <li>
                     <a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-primary text-black" href="#">
-                        <img src="./assets/icons/trending.svg" width="24" height="24" alt="" />
+                        <img src={Treding} width="24" height="24" alt="" />
                         <span>Trending</span>
                     </a>
                 </li>
                 <li>
                     <a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-                        <img src="./assets/icons/newRelease.svg" width="24" height="24" alt="" />
+                        <img src={NewRelease} width="24" height="24" alt="" />
                         <span>New Releases</span>
                     </a>
                 </li>
                 <li>
                     <a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-                        <img src="./assets/icons/commingSoon.svg" width="24" height="24" alt="" />
+                        <img src={CommingSoon} width="24" height="24" alt="" />
                         <span>Coming Soon</span>
                     </a>
                 </li>
                 <li>
                     <a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-                        <img src="./assets/icons/favourite.svg" width="24" height="24" alt="" />
+                        <img src={Favourite} width="24" height="24" alt="" />
                         <span>Favourites</span>
                     </a>
                 </li>
                 <li>
                     <a className="flex items-center space-x-2 px-5 py-3.5 rounded-lg" href="#">
-                        <img src="./assets/icons/watchLater.svg" width="24" height="24" alt="" />
+                        <img src={WatchLater} width="24" height="24" alt="" />
                         <span>Watch Later</span>
                     </a>
                 </li>
