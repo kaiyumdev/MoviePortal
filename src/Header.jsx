b3272c9d15220a6f4e9c321dd1e1c4679473b5b5
kaiyumdev <afrankaiyum34@gmail.com>
2024-11-22 01:05:14 +0600

    feat(cardDetails): show cardDetails when click card logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,37 +1,47 @@
+/* eslint-disable no-unused-vars */
 import Logo from "../src/assets/logo.svg";
 import Ring from "../src/assets/ring.svg";
 import Moon from "../src/assets/icons/moon.svg";
 import ShoppingCart from "../src/assets/shopping-cart.svg";
+import { useState } from "react";
+import CardDetails from "./cine/CardDetails";
 
 const Header = () => {
-    return (
-        <div>
-            <header>
-                <nav className="container flex items-center justify-between space-x-10 py-6">
-                    <a href="index.html">
-                        <img src={Logo} width="139" height="26" alt="" />
-                    </a>
+    const [showCard, setShowCard] = useState(false);
 
-                    <ul className="flex items-center space-x-5">
-                        <li>
-                            <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                                <img src={Ring} width="24" height="24" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                                <img src={Moon} width="24" height="24" alt="" />
-                            </a>
-                        </li>
-                        <li>
-                            <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                                <img src={ShoppingCart} width="24" height="24" alt="" />
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+    const handleShowCard = () => {
+        setShowCard(true);
+    }
+
+    return (
+        <header>
+            {
+                showCard && <CardDetails></CardDetails>
+            }
+            <nav className="container flex items-center justify-between space-x-10 py-6">
+                <a href="index.html">
+                    <img src={Logo} width="139" height="26" alt="" />
+                </a>
+
+                <ul className="flex items-center space-x-5">
+                    <li>
+                        <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                            <img src={Ring} width="24" height="24" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                            <img src={Moon} width="24" height="24" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#" onClick={handleShowCard}>
+                            <img src={ShoppingCart} width="24" height="24" alt="" />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
