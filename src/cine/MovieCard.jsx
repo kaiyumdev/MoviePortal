3fdb1384200b9ff5b36306875d2af0ba58ada069
kaiyumdev <afrankaiyum34@gmail.com>
2024-11-21 19:42:41 +0600

    feat(rating): implements dynamic rating using rating component

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { getImgUrl } from "../utils/cine-utility.js"
+import Rating from "./Rating.jsx"
 const MovieCard = ({ movie }) => {
     return (
         <figure id={movie.id} className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
@@ -8,11 +9,7 @@ const MovieCard = ({ movie }) => {
                 <h3 className="text-xl mb-1">{movie.title}</h3>
                 <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
                 <div className="flex items-center space-x-1 mb-5">
-                    <img src="./assets/star.svg" width="14" height="14" alt="" />
-                    <img src="./assets/star.svg" width="14" height="14" alt="" />
-                    <img src="./assets/star.svg" width="14" height="14" alt="" />
-                    <img src="./assets/star.svg" width="14" height="14" alt="" />
-                    <img src="./assets/star.svg" width="14" height="14" alt="" />
+                    <Rating value={movie.rating}></Rating>
                 </div>
                 <a className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                     href="#">
