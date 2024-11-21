b3213538af68d10815256edc94c756181bf6a175
kaiyumdev <afrankaiyum34@gmail.com>
2024-11-21 11:46:37 +0600

    feat(movieList): create movieList to add under sidebar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import MovieList from './cine/MovieList'
 import Header from './Header'
 import Sdidebar from './Sdidebar'
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sdidebar></Sdidebar>
+          <MovieList></MovieList>
         </div>
       </main>
     </>
