d0d25b66f77c611af68b3e21cc4926fb8db2c7a2
kaiyumdev <afrankaiyum34@gmail.com>
2024-11-22 11:17:55 +0600

    feat(cardData): pass cardData using contextApi

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import { useState } from 'react'
 import MovieList from './cine/MovieList'
+import { MovieContext } from './context'
 import Footer from './Footer'
 import Header from './Header'
 import Sdidebar from './Sdidebar'
 function App() {
+  const [cardData, setCardData] = useState([]);
 
   return (
     <>
-      <Header></Header>
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sdidebar></Sdidebar>
-          <MovieList></MovieList>
-        </div>
-      </main>
-      <Footer></Footer>
+      <MovieContext.Provider value={{ cardData, setCardData }}>
+        <Header></Header>
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sdidebar></Sdidebar>
+            <MovieList></MovieList>
+          </div>
+        </main>
+        <Footer></Footer>
+      </MovieContext.Provider>
     </>
   )
 }
