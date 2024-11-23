7207ddb07b23395edf2cd2553ddcfb07afcec090
kaiyumdev <afrankaiyum34@gmail.com>
2024-11-23 14:36:30 +0600

    feat(darkMode): implements darkMode & lightMode successfully

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react"
 import MovieList from "./cine/MovieList"
 import Footer from "./Footer"
 import Header from "./Header"
 import Sdidebar from "./Sdidebar"
+import { ThemeContext } from "./context"
 
 const Page = () => {
+    const { darkMode } = useContext(ThemeContext);
     return (
-        <div>
+        <div className={`h-full w-full ${darkMode ? "dark" : ""}`}>
             <Header></Header>
             <main>
                 <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
